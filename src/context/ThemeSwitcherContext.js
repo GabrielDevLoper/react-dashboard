05feaa68621eedde3e5d94b980c4263a3b116be9
Gabriel <gabriel.devloper@gmail.com>
2021-01-14 14:58:05 -0300

    criação do custom hook useLocalStorage, desacoplando da context.

--- src/context/ThemeSwitcherContext.js
+++ src/context/ThemeSwitcherContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext } from "react";
 import {
   deepOrange,
   deepPurple,
@@ -7,6 +7,7 @@ import {
 } from "@material-ui/core/colors";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { colors } from "@material-ui/core";
+import { useLocalStorage } from "../customHooks/useLocalStorage";
 
 const Context = createContext();
 
@@ -112,40 +113,4 @@ export function withThemeProvider(Component) {
     );
   };
   return withProvider(WrapperComponent);
-}
-
-function useLocalStorage(key, initialValue) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error);
-      return initialValue;
-    }
-  });
-
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
-  const setValue = (value) => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      // Save state
-      setStoredValue(valueToStore);
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.log(error);
-    }
-  };
-
-  return [storedValue, setValue];
 }
