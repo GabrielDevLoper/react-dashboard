920de9f46ab97b814424db1d9b4ea50941a3554e
Gabriel <gabriel.devloper@gmail.com>
2021-01-18 15:53:03 -0300

    dashboard concluído.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Router } from "react-router-dom";
 import Routes from "./routes";
-import "./App.css";
 import history from "./history";
 
 import { withThemeProvider } from "./context/ThemeSwitcherContext";
